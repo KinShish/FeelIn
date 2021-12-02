70dfd15379473e874a3f01c784bcd49413f50876
Прохач Дмитрий <dimas999000pro@mail.ru>
2021-12-02 22:16:52 +0300

    накидал макет обучения

--- feeling_frontend/src/main.js
+++ feeling_frontend/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import router from './config/router'
 import VueRouter from 'vue-router'
 import axios from 'axios'
-import { BootstrapVue } from 'bootstrap-vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -12,6 +12,7 @@ Vue.config.productionTip = false
 import VueAxios from 'vue-axios'
 
 Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 Vue.use(VueAxios, axios)
 Vue.use(VueRouter)
 const app = {
